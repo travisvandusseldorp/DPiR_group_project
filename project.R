6a991b6155bfe662493ee8bc124268b95f4256dc
travisvandusseldorp <54683549+travisvandusseldorp@users.noreply.github.com>
2019-11-01 21:41:56 -0500

    Theme and formatting
    Created a theme template to use for all our graphs and a bunch of reformatting

--- project.R
+++ project.R
@@ -11,21 +11,37 @@ library(scales)
 library(tidyverse)
 library(forcats)
 
-# Dataframe containing conferences as column names and schools as the data, used to add the conferenceerence to the player, 
-# NA's added so all columns have equal rows
-conferences_df <- data.frame("Big_Ten" = c("Iowa", "Ohio St.", "Wisconsin", "Penn St.", "Michigan", "Northwestern", "Michigan St.", "Maryland",
-                                           "Purdue", "Minnesota", "Indiana", "Rutgers", "Illinois", "Nebraska"),
-                             "SEC" = c("Alabama", "LSU", "Georgia", "Auburn", "Arkansas", "Mississippi St.", "Texas A&M", "Florida", "South Carolina",
-                                       "Mississippi", "Missouri", "Kentucky", "Vanderbilt", "Tennessee"),
-                             "Pac_12" = c("USC", "Oregon", "Stanford", "Washington", "Arizona", "Utah", "Colorado", "Washington St.", "California",
-                                          "Arizona St.", "UCLA", "Oregon St.", "NA", "NA"),
-                             "ACC"= c("Clemson", "Miami (FL)", "Virginia Tech", "Boston Col.", "Florida St.", "Duke", "Pittsburgh", "Wake Forest",
-                                      "North Carolina St.", "Syracuse", "North Carolina", "Virginia", "Georgia Tech", "Louisville"),
-                             "Big_12" = c("Texas", "Oklahoma", "West Virginia", "TCU", "Oklahoma St.", "Iowa St.", "Kansas St.", "Texas Tech", "Baylor",
-                                          "Kansas", "NA", "NA", "NA", "NA"))
+# Dataframe containing conferences as column names and schools as the data, used
+# to add the conferenceerence to the player, NA's added so all columns have equal rows
+
+conferences_df <- data.frame(
+                    "Big_Ten" = c(
+                      "Iowa", "Ohio St.", "Wisconsin", "Penn St.", "Michigan",
+                      "Northwestern", "Michigan St.", "Maryland", "Purdue",
+                      "Minnesota", "Indiana", "Rutgers", "Illinois", "Nebraska"),
+                    "SEC" = c(
+                      "Alabama", "LSU", "Georgia", "Auburn", "Arkansas",
+                      "Mississippi St.", "Texas A&M", "Florida", "South Carolina",
+                      "Mississippi", "Missouri", "Kentucky", "Vanderbilt",
+                      "Tennessee"),
+                    "Pac_12" = c(
+                      "USC", "Oregon", "Stanford", "Washington", "Arizona", 
+                      "Utah", "Colorado", "Washington St.", "California",
+                      "Arizona St.", "UCLA", "Oregon St.", "NA", "NA"),
+                    "ACC"= c(
+                      "Clemson", "Miami (FL)", "Virginia Tech", "Boston Col.",
+                      "Florida St.", "Duke", "Pittsburgh", "Wake Forest",
+                      "North Carolina St.", "Syracuse", "North Carolina",
+                      "Virginia", "Georgia Tech", "Louisville"),
+                    "Big_12" = c(
+                      "Texas", "Oklahoma", "West Virginia", "TCU", "Oklahoma St.",
+                      "Iowa St.", "Kansas St.", "Texas Tech", "Baylor","Kansas",
+                      "NA", "NA", "NA", "NA"))
 
 # Function to add the conferencerence the player went to school in to the draft results df
-add_conferences <- function(c) { draft_results$Conference[draft_results$College %in% conferences_df[[c]]] <<- c }
+add_conferences <- function(c) { 
+  draft_results$Conference[draft_results$College %in% conferences_df[[c]]] <<- c 
+}
 
 # Empty list to hold each years data frame
 draft_results <- list()
@@ -47,7 +63,8 @@ for(i in 2014:2018) {
 draft_results <- do.call(rbind, draft_results)
 
 # Rename the columns
-colnames(draft_results) <- c("Round", "Pick", "Team", "Player", "Position", "Age", "College", "Year")
+colnames(draft_results) <- c("Round", "Pick", "Team", "Player", "Position", 
+                             "Age", "College", "Year")
 
 # Add conferenceerence column to the draft results and set to other
 draft_results$Conference <- "Other"
@@ -57,29 +74,46 @@ conferences <- list("SEC", "Big_Ten", "SEC", "Pac_12", "ACC", "Big_12")
 
 # Call the add_conference function, using lapply to pass a list
 lapply(conferences, add_conferences)
-print(draft_results$Position)
 
 # Group and overwrite offensive and defensive line positions to "OL" and "DL"
-draft_results$Position[draft_results$Position == "C" | draft_results$Position == "G" | draft_results$Position == "T"] <- "OL"
-draft_results$Position[draft_results$Position == "DT" | draft_results$Position == "DE" | draft_results$Position == "NT"] <- "DL" 
-draft_results$Position[draft_results$Position == "ILB" | draft_results$Position == "OLB"| draft_results$Position == "LB"]<- "LB"
-draft_results$Position[draft_results$Position == "DB" | draft_results$Position == "CB"| draft_results$Position == "S"]<- "DB"
+draft_results$Position[draft_results$Position == "C" |
+                       draft_results$Position == "G" |
+                       draft_results$Position == "T"] <- "OL"
+draft_results$Position[draft_results$Position == "DT" |
+                       draft_results$Position == "DE" |
+                       draft_results$Position == "NT"] <- "DL" 
+draft_results$Position[draft_results$Position == "ILB" |
+                       draft_results$Position == "OLB" |
+                       draft_results$Position == "LB"] <- "LB"
+draft_results$Position[draft_results$Position == "DB" |
+                       draft_results$Position == "CB" |
+                       draft_results$Position == "S"] <- "DB"
 
 # Group by player and postion, return df with total players drafted
-df2 <- draft_results %>% group_by(Conference, Position) %>% summarize(Players_Drafted = n())
+df2 <- draft_results %>% 
+  group_by(Conference, Position) %>%
+  summarize(Players_Drafted = n())
 
 # Group by specific conference (ACC) and position, return with total players drafted; sorts by players drafted
-ACC <- draft_results %>% group_by(Conference ="ACC", Position) %>% summarize(Players_Drafted = n())
+ACC <- draft_results %>% 
+  group_by(Conference ="ACC", Position) %>%
+  summarize(Players_Drafted = n())
 ACC <- ACC[order(-ACC$Players_Drafted),]
 
 # Group by specific conference (ACC) and position, return with total players drafted by year
-ACC_Year <- draft_results %>% group_by(Conference = "ACC", Position, Year) %>% summarize(Players_Drafted = n())
+ACC_Year <- draft_results %>%
+  group_by(Conference = "ACC", Position, Year) %>%
+  summarize(Players_Drafted = n())
 
 # Group by specific conference (ACC) and position, return with total players drafted by year
-Overall_ACC <- draft_results %>% group_by(Conference = "ACC", Team, Year,) %>% summarize(Players_Drafted = n())
+Overall_ACC <- draft_results %>%
+  group_by(Conference = "ACC", Team, Year,) %>%
+  summarize(Players_Drafted = n())
 
 # Function to return the ranking in descending order of players draft at a postion by conference
-most_drafted_at_position <- function(x) { df2 %>% filter(Position == x) %>% arrange(desc(Players_Drafted)) }
+most_drafted_at_position <- function(x) {
+  df2 %>% filter(Position == x) %>% arrange(desc(Players_Drafted)) 
+}
 
 # Run most_drafted_at_postion function on the positions of interest
 wrs_drafted <- most_drafted_at_position("WR")
@@ -90,19 +124,54 @@ dls_drafted <- most_drafted_at_position("DL")
 lbs_drafted <- most_drafted_at_position("LB")
 dbs_drafted <- most_drafted_at_position("DB")
 
-number_players_drafted <- rbind(wrs_drafted,rbs_drafted,qbs_drafted,ols_drafted,dls_drafted,lbs_drafted,dbs_drafted)
+number_players_drafted <- rbind(wrs_drafted, rbs_drafted, qbs_drafted, 
+                                ols_drafted,dls_drafted,lbs_drafted,dbs_drafted)
 
-ggplot(data = number_players_drafted, aes_(x=number_players_drafted$Position, y=number_players_drafted$Players_Drafted, group=number_players_drafted$Conference)) +
-  geom_line(aes(color=number_players_drafted$Conference)) +
-  geom_point(aes(color=number_players_drafted$Conference))
 
+#### Graphs
+
+# Creating a standard theme for the project to avoid reformating each graph
+theme_project <- function() {
+  theme_minimal() %+replace%
+    theme(
+      axis.text.x = element_text(size = 13, face="bold"), 
+      axis.text.y = element_text(size = 13, face="bold"), 
+      plot.title=element_text(size=18, face="bold", hjust = 0.5)
+    )
+}
+
+ggplot(data = number_players_drafted, 
+       aes_(x=number_players_drafted$Position, 
+            y=number_players_drafted$Players_Drafted, 
+            group=number_players_drafted$Conference)) +
+       geom_line(aes(color=number_players_drafted$Conference)) +
+       geom_point(aes(color=number_players_drafted$Conference))
 
 # Aaron: Just messing with these, not finished graphs
 
-ggplot(data = number_players_drafted, aes_(x=number_players_drafted$Conference, y=number_players_drafted$Players_Drafted)) +
-  geom_line(aes(color=number_players_drafted$Players_Drafted))
+ggplot(data = number_players_drafted, 
+       aes_(x=number_players_drafted$Conference,
+            y=number_players_drafted$Players_Drafted)) +
+       geom_line(aes(color=number_players_drafted$Players_Drafted))
 
-ggplot(data = number_players_drafted, aes_(x = number_players_drafted$Conference[number_players_drafted$Conference == "Big_Ten"], y=number_players_drafted$Players_Drafted, group=number_players_drafted$Position))+geom_line(aes(color=number_players_drafted$Conference))
+ggplot(data = number_players_drafted,
+      aes_(x = number_players_drafted$Conference
+           [number_players_drafted$Conference == "Big_Ten"],
+           y=number_players_drafted$Players_Drafted,
+           group=number_players_drafted$Position)) +
+      geom_line(aes(color=number_players_drafted$Conference))
+
+#ggplot(data = number_players_drafted,
+#       aes_(x = subset(number_players_drafted, Conference == "Big_Ten"), 
+#            y = number_players_drafted$Players_Drafted,
+#            group = number_players_drafted$Position))
+#              
+#              number_players_drafted$Conference
+#            [number_players_drafted$Conference == "Big_Ten"],
+#            y=number_players_drafted$Players_Drafted,
+#            group=number_players_drafted$Position)) +
+#  geom_line(aes(color=number_players_drafted$Conference))
+
 
 
 
@@ -110,53 +179,80 @@ ggplot(data = number_players_drafted, aes_(x = number_players_drafted$Conference
 
 #1: Players Drafted by Year; showing all conferences.  This is my first plot.  Need to update title font.
 c <- qplot(Year, data = draft_results, geom = "bar", facets = . ~ Conference)
-c <- c + theme_minimal() + theme(axis.text.x = element_text(size = 6), axis.text.y = element_text(size = 12, face="bold"), title=element_text(size=14,face="bold"))
-c <- c + labs(x = "Year", title = "Players Drafted by Year")
-c <- c + theme(plot.title = element_text(hjust = 0.5))
-c
+
+c <- c + labs(title = "Number of Players Drafted by Year") + 
+         theme_project()
+c 
+
 ggsave('Players_Drafted_by_Year.png')
 
 #2: Change in data over year by position. Will be on same slide as plot #1.
-d <- ggplot(data = ACC_Year, aes_(x=ACC_Year$Year, y=ACC_Year$Players_Drafted, group=ACC_Year$Position)) +
-  geom_line(aes(color=ACC_Year$Position)) + geom_point(aes(color=ACC_Year$Position))
-d <- d + theme_minimal()
+d <- ggplot(data = ACC_Year,
+            aes_(x=ACC_Year$Year,
+                 y=ACC_Year$Players_Drafted,
+                 group=ACC_Year$Position)) +
+            geom_line(aes(color=ACC_Year$Position)) +
+            geom_point(aes(color=ACC_Year$Position))
+
+d <- d + theme_minimal() +
+         labs(color = "Position") +
+         scale_y_continuous(name="Players Drafted", limits=c(0, 60))
 d
+
 ggsave('Change_in_Position_by_Year.png')
 
 #3: Position Most Drafted in ACC
-acc <- draft_results %>% filter(Conference == 'ACC')
+acc <- draft_results %>%
+        filter(Conference == 'ACC')
+
 p <- acc %>% count(Position) %>%
   mutate(Position = fct_reorder(Position, n, .desc = TRUE)) %>%
-  ggplot(aes(x = Position, y = n)) + geom_bar(stat = 'identity', aes(fill = Position), show.legend = FALSE)
-p <- p + theme_minimal() + theme(axis.text.x = element_text(size = 13, face="bold"), axis.text.y = element_text(size = 13, face="bold"), title=element_text(size=18,face="bold"))
-p <- p + scale_fill_viridis_d(direction = 1)
-p <- p + labs(title = "Positions Most Drafted in ACC", x = "", y = "")
-p <- p + theme(plot.title = element_text(hjust = 0.5))
+  ggplot(aes(x = Position, y = n)) +
+  geom_bar(stat = 'identity', aes(fill = Position), show.legend = FALSE)
+
+p <- p + labs(title = "Positions Most Drafted in ACC", x = "", y = "") +
+  theme_project()
+
+# Try this line and see if you like the look better, I think horrizontal looks better if we put one graph on a slide but vertical if two
+#p <- p + scale_fill_viridis_d(direction = 1)
+
 p
+
 ggsave('ACC_players_by_position.png')
 
 
 #4: College Most Drafted in ACC
-p1 <- acc %>% count(College) %>%
-  mutate(College = fct_reorder(College, n, .desc = TRUE)) %>% top_n(5) %>% 
-  ggplot(aes(x = College, y = n)) + geom_bar(stat = 'identity', aes(fill = College), show.legend = FALSE)
-p1 <- p1 + theme_minimal() + theme(axis.text.x = element_text(size = 13, face="bold"), axis.text.y = element_text(size = 13, face="bold"), title=element_text(size=16,face="bold"))
-p1 <- p1 + scale_fill_viridis_d(direction = 1)
-p1 <- p1 + labs(title = "Colleges Most Drafted in ACC", x = "", y = "")
-p1 <- p1 + theme(plot.title = element_text(hjust = 0.5))
+p1 <- acc %>% 
+  count(College) %>%
+  mutate(College = fct_reorder(College, n, .desc = TRUE)) %>%
+  top_n(5) %>% 
+  ggplot(aes(x = College, y = n)) +
+  geom_bar(stat = 'identity', 
+           aes(fill = College), show.legend = FALSE) +
+  labs(title = "Colleges Most Drafted in ACC", x = "", y = "") +
+  theme_project()
+
+#p1 <- p1 + scale_fill_viridis_d(direction = 1)
+
 p1
 ggsave('ACC_players_Drafted_by_College_Attended.png')
 
 
 #5: Teams to most likely draft Players out of ACC
-acc <- draft_results %>% filter(Conference == 'ACC')
-p2 <- acc %>% count(Team) %>%
-  mutate(Team = fct_reorder(Team, n, .desc = TRUE)) %>% top_n(5) %>% 
-  ggplot(aes(x = Team, y = n)) + geom_bar(stat = 'identity', aes(fill = Team), show.legend = FALSE)
-p2 <- p2 + theme_minimal() + theme(axis.text.x = element_text(size = 12, face="bold"), axis.text.y = element_text(size = 12, face="bold"), title=element_text(size=14,face="bold"))
-p2 <- p2 + scale_fill_viridis_d(direction = 1)
-p2 <- p2 + labs(title = "Teams Most Likely to Draft an ACC Player", x = "", y = "")
-p2 <- p2 + theme(plot.title = element_text(hjust = 0.5))
+acc <- draft_results %>% 
+  filter(Conference == 'ACC')
+
+p2 <- acc %>% 
+    count(Team) %>%
+    mutate(Team = fct_reorder(Team, n, .desc = TRUE)) %>% 
+    top_n(5) %>% 
+    ggplot(aes(x = Team, y = n)) + 
+    geom_bar(stat = 'identity', 
+             aes(fill = Team), show.legend = FALSE) +
+    labs(title = "Teams Most Likely to Draft an ACC Player", x = "", y = "") +
+    theme_project()
+
+# scale_fill_viridis_d(direction = 1)
 p2
 ggsave('ACC_players_by_Teams.png')
 
@@ -174,9 +270,3 @@ ggplot(data = Overall_ACC, aes_(x=Overall_ACC$Year, y=Overall_ACC$Players_Drafte
 ggplot(data=Overall_ACC, aes_(x=Overall_ACC$Year, y=Overall_ACC$Players_Drafted, group=Overall_ACC$Team)) +
   geom_bar(stat="identity", position=position_dodge())
 
-
- 
-
-
-
-       
